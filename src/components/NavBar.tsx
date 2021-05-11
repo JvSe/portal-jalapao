596d8b505dab5569556275d588f5abe1e6086670
Joao Vitor Soares <joaovitorsoares12@gmail.com>
2021-05-11 13:32:31 -0300

    Iniciando o repositorio

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '../styles/components/NavBar.module.scss';
+
+export default function NavBar() {
+    return(
+        <div className={styles.container}>
+            <h1>Nav Bar</h1>
+            <h1> Menu hamburger</h1>
+        </div>
+    )
+}
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainPages from './page/MainPages';
+import NavBar from './components/NavBar';
+
+export default function Main() {
+    return(
+        <>
+            <NavBar />
+            <MainPages />
+        </>
+    )
+}
